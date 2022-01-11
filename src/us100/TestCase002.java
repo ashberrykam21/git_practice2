162217852c448f9c61dc4987b90d385a3ddf7d95
AnastasiaM <kamnastya21@yandex.ru>
2022-01-11 11:48:28 -0500

    task 2 update 2

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -4,5 +4,7 @@ public class TestCase002 {
     public static void main(String[] args) {
         System.out.println("Code master!");
 
+        System.out.println("Writing codes is my hobby!");
+
     }
 }
